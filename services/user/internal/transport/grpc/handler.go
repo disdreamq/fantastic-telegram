ad43ad87e19f77b71f6cd13985d18b6156036fab
disdreamq <vlaseyar@gmail.com>
2026-07-31 14:11:25 +0300

    added trace id to handler

--- services/user/internal/transport/grpc/handler.go
+++ services/user/internal/transport/grpc/handler.go
@@ -3,20 +3,21 @@ package grpc
 import (
 	"context"
 
-	"github.com/disdreamq/fantastic-telegram/services/user/internal/service"
+	"github.com/disdreamq/fantastic-telegram/services/user/internal/port"
 	pb "github.com/disdreamq/fantastic-telegram/services/user/proto"
 	"google.golang.org/grpc"
 )
 
 type userServer struct {
 	pb.UnimplementedUserServiceServer
-	uSVC *service.UserService
+	uSVC port.UserService
 }
 
 func (s *userServer) GetUser(ctx context.Context, in *pb.GetUserRequest, opts ...grpc.CallOption) (*pb.GetUserResponse, error) {
+	ctx = context.WithValue(ctx, "trace_id", in.Request_ID)
 	u, err := s.uSVC.GetByID(ctx, in.UserId)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.GetUserResponse{Id: u.ID, Username: u.Username, Exists: true}, nil
+	return &pb.GetUserResponse{ID: u.ID, Username: u.Username, Exists: true, Request_ID: in.Request_ID}, nil
 }
