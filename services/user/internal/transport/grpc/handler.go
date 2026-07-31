d7a6bac0df86899be4efdd27fc6a5eb720e5e64f
disdreamq <vlaseyar@gmail.com>
2026-07-31 17:55:43 +0300

    now can validate token with grpc

--- services/user/internal/transport/grpc/handler.go
+++ services/user/internal/transport/grpc/handler.go
@@ -2,22 +2,31 @@ package grpc
 
 import (
 	"context"
+	"strings"
 
 	"github.com/disdreamq/fantastic-telegram/services/user/internal/port"
 	pb "github.com/disdreamq/fantastic-telegram/services/user/proto"
-	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type userServer struct {
 	pb.UnimplementedUserServiceServer
-	uSVC port.UserService
+	p port.TokenProvider
 }
 
-func (s *userServer) GetUser(ctx context.Context, in *pb.GetUserRequest, opts ...grpc.CallOption) (*pb.GetUserResponse, error) {
-	ctx = context.WithValue(ctx, "trace_id", in.Request_ID)
-	u, err := s.uSVC.GetByID(ctx, in.UserId)
-	if err != nil {
-		return nil, err
+func (s *userServer) ValidateToken(ctx context.Context, in *pb.ValidateTokenRequest) (*pb.ValidateTokenResponse, error) {
+	authValues := strings.Split(in.Token, " ")
+	if len(authValues) == 0 {
+		return nil, status.Errorf(codes.Unauthenticated, "missing auth header")
 	}
-	return &pb.GetUserResponse{ID: u.ID, Username: u.Username, Exists: true, Request_ID: in.Request_ID}, nil
+	if len(authValues) != 2 || authValues[0] != "Bearer" {
+		return nil, status.Errorf(codes.Unauthenticated, "missing auth header")
+	}
+	token := authValues[1]
+	payload, err := s.p.ValidateToken(token)
+	if err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "invalid token")
+	}
+	return &pb.ValidateTokenResponse{Valid: true, UserID: payload.Claims.UserID, UserEmail: payload.Claims.Email}, nil
 }
