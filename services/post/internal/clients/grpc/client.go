ff8e330486a3ded3ab94dd2dfe0c1ae8ac238460
disdreamq <vlaseyar@gmail.com>
2026-08-03 18:06:52 +0300

    added grpc client for posts

---
+++ services/post/internal/clients/grpc/client.go
@@ -0,0 +1,56 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/disdreamq/fantastic-telegram/services/post/internal/domain"
+	pb "github.com/disdreamq/fantastic-telegram/services/post/proto"
+)
+
+type grpcClient struct {
+	grpcClient pb.UserServiceClient
+	Conn       *grpc.ClientConn
+	timeout    time.Duration
+}
+
+func NewGRPCClient(serverAddr string, opts ...grpc.DialOption) (*grpcClient, error) {
+	conn, err := grpc.NewClient(serverAddr,
+		append([]grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}, opts...)...,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &grpcClient{
+		grpcClient: pb.NewUserServiceClient(conn),
+		Conn:       conn,
+		timeout:    5 * time.Second,
+	}, nil
+}
+
+func (c *grpcClient) Close() error {
+	return c.Conn.Close()
+}
+
+func (c *grpcClient) ValidateToken(ctx context.Context, token string) (*domain.Claims, error) {
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+
+	req := &pb.ValidateTokenRequest{
+		Token: token,
+	}
+
+	resp, err := c.grpcClient.ValidateToken(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(req, resp, resp.UserID, resp.UserEmail)
+	return &domain.Claims{
+		UserID: resp.UserID,
+		Email:  resp.UserEmail,
+	}, nil
+}
