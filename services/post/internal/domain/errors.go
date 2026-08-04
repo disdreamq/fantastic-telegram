02f8b0b6e6524cd875c971c2e161caae74fb6ab5
disdreamq <vlaseyar@gmail.com>
2026-08-04 13:14:50 +0300

    added token validation error

--- services/post/internal/domain/errors.go
+++ services/post/internal/domain/errors.go
@@ -7,4 +7,7 @@ var (
 	ErrInvalidContent = errors.New("Invalid content")
 	ErrInvalidUserId  = errors.New("Invalid user ID")
 	ErrInvalidID      = errors.New("Invalid ID")
+
+	// grpc
+	ErrInvalidToken = errors.New("Invalid token")
 )
